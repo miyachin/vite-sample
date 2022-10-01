bd60ba1dc3e3475f0fe96ed74dca7ecbc7c98595
MIYACHIN <miyachi.910807@gmail.com>
2022-10-02 02:41:56 +0900

    ogp

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    axios(
+      "https://manage-expence-api-prod.herokuapp.com/api/group/group-id-sample-jp"
+    ).then((res: any) => {
+      const metaTitleTag = document.querySelector(
+        'meta[property="og:title"]'
+      ) as HTMLMetaElement;
+      if (metaTitleTag) {
+        metaTitleTag.content = res.data["group_name"];
+      }
+    });
+  });
 
   return (
     <div className="App">
@@ -28,7 +42,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
